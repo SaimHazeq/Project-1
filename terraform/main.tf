8b2fb837ec6ac046a1acd3b63d606794e03151a8
SaimHazeq <233766529+SaimHazeq@users.noreply.github.com>
2026-03-27 18:16:51 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,33 +1,20 @@
-############################
-# VPC
-############################
 module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.0.0"
+  source = "./vpc"
 
-  name = "devops-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["ap-south-1a", "ap-south-1b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
-
-  enable_nat_gateway = true
-  single_nat_gateway = true
-
-  tags = {
-    Project = "DevOps"
-  }
+  project_name = var.project_name
 }
 
-############################
-# EKS CLUSTER
-############################
+module "ecr" {
+  source = "./ecr"
+
+  project_name = var.project_name
+}
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.0.0"
+  version = "~> 20.0"
 
-  cluster_name    = var.cluster_name
+  cluster_name    = "${var.project_name}-eks"
   cluster_version = "1.29"
 
   vpc_id     = module.vpc.vpc_id
@@ -37,15 +24,10 @@ module "eks" {
 
   eks_managed_node_groups = {
     default = {
-      desired_size = 2
-      min_size     = 1
-      max_size     = 3
-
       instance_types = ["t3.medium"]
+      desired_size   = 2
+      min_size       = 1
+      max_size       = 3
     }
-  }
-
-  tags = {
-    Project = "DevOps"
   }
 }
