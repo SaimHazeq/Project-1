c52525f770876c7b465a96288fc31158be3d349f
SaimHazeq <233766529+SaimHazeq@users.noreply.github.com>
2026-03-27 17:53:24 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,6 +2,6 @@ output "cluster_name" {
   value = module.eks.cluster_name
 }
 
-output "ecr_repo_url" {
-  value = module.ecr.repository_url
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
 }
