f74c0740b63612e2058fd88ec4562936c1e29c59
SaimHazeq <233766529+SaimHazeq@users.noreply.github.com>
2026-03-27 17:04:32 +0530

    Create main.tf

---
+++ terraform/eks/main.tf
@@ -0,0 +1,15 @@
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "devops-eks"
+  cluster_version = "1.29"
+
+  subnet_ids = var.subnet_ids
+  vpc_id     = var.vpc_id
+
+  node_groups = {
+    default = {
+      desired_capacity = 2
+      instance_types   = ["t3.medium"]
+    }
+  }
+}
