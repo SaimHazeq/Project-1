a1aefbaca1b5bddbb93d88d3978fadba44f2a6ed
SaimHazeq <233766529+SaimHazeq@users.noreply.github.com>
2026-03-27 22:02:59 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,7 @@ module "eks" {
   version = "~> 20.0"
 
   cluster_name    = "${var.project_name}-eks"
-  cluster_version = "1.35"
+  cluster_version = "1.33"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
