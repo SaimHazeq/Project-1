626534e586eb1b9d4dd2efb5cdcba5fbfbbbc6e2
SaimHazeq <233766529+SaimHazeq@users.noreply.github.com>
2026-04-01 00:01:33 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,14 +2,14 @@ output "vpc_id" {
   value = module.vpc.vpc_id
 }
 
-output "private_subnets" {
-  value = module.vpc.private_subnets
-}
-
-output "ecr_repo_url" {
-  value = module.ecr.repository_url
+output "eks_cluster_endpoint" {
+  value = module.eks.cluster_endpoint
 }
 
 output "eks_cluster_name" {
   value = module.eks.cluster_name
+}
+
+output "kubeconfig_command" {
+  value = "aws eks update-kubeconfig --region ap-south-1 --name ${module.eks.cluster_name}"
 }
